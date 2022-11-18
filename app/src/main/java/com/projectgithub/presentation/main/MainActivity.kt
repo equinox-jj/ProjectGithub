6010653c3344b280584d69ce12d3d176c2bc421b
Joshua Jeremia <79564114+equinox-jj@users.noreply.github.com>
2022-11-18 20:12:05 +0700

    Fix navigation screen.

--- app/src/main/java/com/projectgithub/presentation/main/MainActivity.kt
+++ app/src/main/java/com/projectgithub/presentation/main/MainActivity.kt
@@ -2,15 +2,37 @@ package com.projectgithub.presentation.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.projectgithub.R
 import com.projectgithub.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var navHostFragment: NavHostFragment
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        val splashScreen = installSplashScreen()
+
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.mainNavHost) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        navController = this.findNavController(R.id.mainNavHost)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
